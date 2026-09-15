583cb173aecdf739b2e8e937236e9761204558cb
Edoardo <edoardo@Edoardos-MacBook-Pro.local>
2026-09-15 21:58:15 +0200

    Implemented unique bookings and sending only appropriate start dates to the user

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -56,6 +56,10 @@ router
     tourController.deleteTour,
   );
 
-router.get('/slug/:slug', authController.isLoggedIn, tourController.getTourBySlug);
+router.get(
+  '/slug/:slug',
+  authController.isLoggedIn,
+  tourController.getTourBySlug,
+);
 
 module.exports = router;
